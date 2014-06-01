e715331bd860cd91d374eb534b73fee1c8df22db
darthredskin <darthredskin@gmail.com>
2014-06-01 10:36:02 -0400

    Basic Functionality

---
+++ Gemfile
@@ -0,0 +1,42 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'rails', '4.0.0'
+
+gem 'rails_12factor', group: :production
+	gem 'pg'
+	
+gem 'devise'
+
+group :production do
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+gem 'sass-rails', '~> 4.0.0'
+
+
+gem 'uglifier', '>= 1.3.0'
+
+
+gem 'coffee-rails', '~> 4.0.0'
+
+
+gem 'jquery-rails'
+
+
+gem 'turbolinks'
+
+gem 'heroku'
+
+	gem 'bootstrap-sass', '~> 3.1.1'
+
+
+
+group :doc do
+ 
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails::Application.config.secret_key_base = 'b9d8f0ebfb27599693ea46a313e7d683be6c501b83ec9aecf59dadb77f739a812a8048d4cba5fb7e7978334a1b6eb39c711260500bf16adc21166f41314db813'
