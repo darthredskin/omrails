e93cead94dd1ba83f07f574bb724c66ca5f03de0
darthredskin <darthredskin@gmail.com>
2014-06-03 10:54:39 -0400

    Fix Bad Code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Omrails::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -80,13 +80,7 @@ Omrails::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  # config/environments/production.rb
-YOURAPPLICATION::Application.configure do
-
-   # your config settings
-
-   config.assets.compile = true
+   
 
    # your other config settings
-end
 end
